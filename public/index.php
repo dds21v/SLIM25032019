e78cb2c3b32532afaa7d3045a85e0f7707a1ef22
Damien <dds21@mail.com>
2019-03-25 16:40:31 +0100

    End of the First Day - Slim/Regex/router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,46 @@
-<?php 
+<?php
 
-echo "Hello!";
+use Slim\App;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
- ?>
+// Autoloader
+require_once dirname(__DIR__) .  '/vendor/autoload.php';
+
+// Création de l'application
+$app = new App();
+
+
+// Création d'un route
+$app->get('/', function (
+    ServerRequestInterface $request,
+    ResponseInterface $response,
+    ?array $args
+) {
+        // On retourne une réponse
+    return $response->getBody()->write('<h1>Bonjour</h1>');
+}) ->setName("homepage");
+
+
+$app->group('/projet', function() {
+    // Création d'une page détail des projets
+    // Nouveauté: On ajoute une variable dans l'URL avec les accolades
+    $this->get("/{id:\d+}", function (ServerRequestInterface $request, ResponseInterface $response,?array $args)
+    {
+            // On retourne une réponse
+        return $response->getBody()->write('<h1>Détail du projet</h1>');
+    }) ->setName("app_project_show");
+
+    $this->get("/creation", function (ServerRequestInterface $request, ResponseInterface $response,?array $args)
+    {
+            // On retourne une réponse
+        return $response->getBody()->write('<h1>Création d\'un projet</h1>');
+    }) ->setName("app_project_create");
+
+});
+
+
+
+
+// création et renvoi de la réponse au navigateur
+$app->run();
